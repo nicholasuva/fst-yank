e7cac3ef20a2bd78619f39bd8df9f8506a5a9df6
Nicholas <uvanicholasj@gmail.com>
2024-03-02 19:47:02 -0500

    add morph tagger draft

---
+++ morph-annotate.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+#this is going to be a script which will essentially take the morphological parse dictionary which was 
+#yanked from the hfst files from giellalt, and take a corpus, and return a morphological info corpus
+#I need to go back to my notes but I think kyle suggested a three column format either from Unimorph or UD
